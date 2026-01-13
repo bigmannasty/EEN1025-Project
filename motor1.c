12b57680334f635b47d992080b8f78a7f8ca711a
ZygiGricius <zygimantas.gricius2@mail.dcu.ie>
2026-01-13 14:29:18 +0000

    Add motor2 control for PWM and phase

--- motor1.c
+++ motor1.c
@@ -1,19 +1,31 @@
 int motor1PWM = 37;
 int motor1Phase = 38;
+int motor2PWM = 39;
+int motor2Phase = 20;
+
 // the setup routine runs once when you press reset:
 void setup() {
   Serial.begin(9600);
+
+  // Wheel initialisation
   pinMode(motor1PWM, OUTPUT);
   pinMode(motor1Phase, OUTPUT);
+  pinMode(motor2PWM, OUTPUT);
+  pinMode(motor2Phase, OUTPUT);
+
  }
 // the loop routine runs over and over again continuously:
 void loop() {
   digitalWrite(motor1Phase, HIGH); //forward
   analogWrite(motor1PWM, 100); // set speed of motor
+  digitalWrite(motor2Phase, HIGH); //forward
+  analogWrite(motor2PWM, 100); // set speed of motor
   Serial.println("Forward"); // Display motor direction
   delay(2000); //2 seconds
   digitalWrite(motor1Phase, LOW); //Backward
   analogWrite(motor1PWM, 100); // set speed of motor
+  digitalWrite(motor2Phase, LOW); //Backward
+  analogWrite(motor2PWM, 100); // set speed of motor
   Serial.println("Backward"); // Display motor direction
   delay(2000); //2 seconds
 }
