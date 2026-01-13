89f11de045512bc7d9cb79a08bc107f7c98d0687
bigmannasty <admirmen@gmail.com>
2026-01-13 10:51:23 +0000

    Create motor1.c
    output pins 4 & 5

---
+++ motor1.c
@@ -0,0 +1,19 @@
+int motor1PWM = 4;
+int motor1Phase = 5;
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(9600);
+  pinMode(motor1PWM, OUTPUT);
+  pinMode(motor1Phase, OUTPUT);
+ }
+// the loop routine runs over and over again continuously:
+void loop() {
+  digitalWrite(motor1Phase, HIGH); //forward
+  analogWrite(motor1PWM, 100); // set speed of motor
+  Serial.println("Forward"); // Display motor direction
+  delay(2000); //2 seconds
+  digitalWrite(motor1Phase, LOW); //Backward
+  analogWrite(motor1PWM, 100); // set speed of motor
+  Serial.println("Backward"); // Display motor direction
+  delay(2000); //2 seconds
+}
