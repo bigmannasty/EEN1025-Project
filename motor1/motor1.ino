ad84f0e9733ae0d182421a7b5e2fa4e504d1208a
bigmannasty <admirmen@gmail.com>
2026-01-27 17:59:01 +0000

    changed weightings and distance sensor code

--- motor1/motor1.ino
+++ motor1/motor1.ino
@@ -1,46 +1,263 @@
+int AnalogValue[5]; //intialise 0s
+int AnalogPin[5] = { 4, 5, 6, 7, 15 };  // keep 8 free for tone O/P music
+
+
+//sonar distance sensor vars
+const int trigPin = 17;   // GPIO15
+const int echoPin = 16;   // GPIO16
+unsigned long duration;
+float distanceCm;
+
+
+/*
+//initialise the optical distance sensor values
+int DistanceValue = 0;
+int distAnalogPin = 16;
+*/
+
+//Initialisation of pins to control motor
 int motor1PWM = 37;
 int motor1Phase = 38;
 int motor2PWM = 39;
 int motor2Phase = 20;
 
-// the setup routine runs once when you press reset:
+//intitialise threshold, base motor speed, and turn gain
+const int WHITE_THRESHOLD = 500;
+
+//Fastest speed and turn_gain mobot can have before errors
+const int speedL = 242;
+const int speedR = 255;
+const int TURN_GAIN = 110;
+
+
+int zeroCount = 0;
+
 void setup() {
   Serial.begin(9600);
 
-  // Wheel initialisation
+  // Wheel pin initialisation
   pinMode(motor1PWM, OUTPUT);
   pinMode(motor1Phase, OUTPUT);
   pinMode(motor2PWM, OUTPUT);
   pinMode(motor2Phase, OUTPUT);
 
- }
-
-
-void motorFWD(int PWM)
-{
-  digitalWrite(motor1Phase, HIGH);
-  digitalWrite(motor2Phase, LOW);
-
-  analogWrite(motor1PWM, PWM);
-  analogWrite(motor2PWM, PWM);
+  //sonar sensor pin initialisation
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  digitalWrite(trigPin, LOW);
 }
 
-void motorBWD(int PWM)
-{
-  digitalWrite(motor1Phase, LOW);
-  digitalWrite(motor2Phase, HIGH);
+//function to drive each motor at controlled pwm
+void motorDrive(int leftPWM, int rightPWM) {
+  leftPWM = constrain(leftPWM, 0, 255);
+  rightPWM = constrain(rightPWM, 0, 255);
+  
+  analogWrite(motor1PWM, leftPWM);
+  analogWrite(motor2PWM, rightPWM);
+}
 
-  analogWrite(motor1PWM, PWM);
-  analogWrite(motor2PWM, PWM);
+//function to set direction of motors
+void motorDir(int dir) {
+  if (dir == 0){
+    digitalWrite(motor1Phase, LOW);
+    digitalWrite(motor2Phase, HIGH);
+  }
+  else {
+    digitalWrite(motor1Phase, HIGH);
+    digitalWrite(motor2Phase, LOW);
+  }
+
+}
+
+//dir:0 cw, 1 anti-cw
+void motorTurn(int dir) { 
+  if (dir == 0){
+    digitalWrite(motor1Phase, HIGH);
+    digitalWrite(motor2Phase, HIGH);
+  }
+  else {
+    digitalWrite(motor1Phase, LOW);
+    digitalWrite(motor2Phase, LOW);
+  }
+  motorDrive(100,100);
 }
 
 
-// the loop routine runs over and over again continuously:
+void sonicSensorTrig() {
+  // trigger pulse
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+}
+
+int weights[5] = {-3, -1, 0, 1, 3}; // set up the weightings gor each sensor
+int correction; // corection var
+bool obstacleDetected; //distance sense boolean
+int activeSensors; //var for detecting nodes
+bool nodeDetected = false;
+
+
 void loop() {
-  motorFWD(100);
-  Serial.println("Forward"); // Display motor direction
-  delay(2000); //2 seconds
-  motorBWD(100);
-  Serial.println("Backward"); // Display motor direction
-  delay(2000); //2 seconds
+
+  /*
+  //check distance sensor
+  DistanceValue = analogRead(distAnalogPin);
+  Serial.print("Distance:");
+  Serial.print(" ");
+  Serial.print(DistanceValue);
+  Serial.println("");
+  if (DistanceValue >= 1000) {
+    motorDrive(0,0);
+    obstacleDetected = true;
+    while (obstacleDetected) {
+      DistanceValue = analogRead(distAnalogPin);
+      if (DistanceValue < 2000) obstacleDetected = false;
+    }
+  }
+  */
+
+  motorDir(0); //set motor direction to forward
+
+
+
+/*
+  sonicSensorTrig();
+  // read echo pulse width (timeout to prevent lockup)
+  duration = pulseIn(echoPin, HIGH, 30000); // 30ms ~ 5m max
+  //if (duration != 0) { distanceCm = (duration * 0.0343f) / 2.0f; }
+
+    
+  Serial.print("Duration: ");
+  Serial.print(duration, 1);
+  Serial.println(" ");
+  */
+  
+
+
+  
+/*
+  if (duration <= 600) {
+    motorDrive(0,0);
+    obstacleDetected = true;
+    while (obstacleDetected) {
+      motorDrive(0,0);
+      sonicSensorTrig();
+      duration = pulseIn(echoPin, HIGH, 30000);
+      Serial.print("Duration: ");
+      Serial.print(duration, 1);
+      Serial.println(" ");
+      if (duration > 800) { obstacleDetected = false; }
+      delay(200);
+    }
+  }
+  //}
+  */
+
+  /*
+
+  if (duration <= 600 && duration > 10) {
+    motorDrive(0,0);
+    obstacleDetected = true;
+    Serial.print("Duration: ");
+    Serial.print(duration, 1);
+    Serial.println(" ");
+    
+    while (obstacleDetected) {
+      sonicSensorTrig();
+      duration = pulseIn(echoPin, HIGH, 30000); // 30ms ~ 5m max
+      Serial.print("Duration: ");
+      Serial.print(duration, 1);
+      Serial.println(" ");
+      if (duration > 800) { obstacleDetected = false; }
+      if (duration < 10) { zeroCount++;}
+      if (zeroCount > 3) {
+        pinMode(echoPin, OUTPUT);
+        digitalWrite(echoPin, LOW);
+        delay(1000);
+        pinMode(echoPin, INPUT);
+        zeroCount = 0;
+      }
+    }
+  }
+
+  */
+
+
+  
+
+  //intialise error and line detect vars
+  int error = 0;
+  bool lineDetected = false;
+  activeSensors = 0;
+
+
+  //Code will retrieve sensor values continuously
+  for (int i = 0; i < 5; i++) {
+    AnalogValue[i] = analogRead(AnalogPin[i]);
+  if (AnalogValue[i] <= WHITE_THRESHOLD) {
+      error += weights[i];
+      activeSensors++;
+      if (i == 2) {lineDetected = true;}
+    }
+  }
+
+  if (activeSensors >= 4) nodeDetected = true; //checking for node
+
+  
+/*
+  //turn-around on node loop
+  if (nodeDetected = true) {
+    motorDrive(100,100);//move forward a bit before turn
+    delay(50);
+    motorTurn(0);
+    delay(500);
+    lineDetected = false;
+    //while mid sensor isnt on line, keep on turnin
+    while (!lineDetected) {
+      int midSensor = analogRead(AnalogPin[2]);
+      if (midSensor <= WHITE_THRESHOLD) lineDetected = true;
+      motorTurn(0);
+    }
+  }
+  */
+  
+
+  //find required correction and set each wheel speed
+  correction = error * TURN_GAIN;
+  int leftSpeed = speedL;
+  int rightSpeed = speedR;
+
+  if (!lineDetected) {
+    leftSpeed += correction;
+    rightSpeed -= correction;
+  }
+
+  //yeah, i drive
+  if (!obstacleDetected) {
+    motorDrive(rightSpeed, leftSpeed);
+  }
 }
+
+/*
+----
+Notes:
+Black >= 2600
+White <= 500
+Sensors range from 200 to 4095 typically
+
+----
+Operation:
+1. Vehicle will not run until it detects the white line in the middle.
+
+2. When middle sensor is below 300, vehicle will begin detecting line and moving forward.
+  2a. Vehicle should automatically adjust angle so it is driving as parallel to white line as possible.
+
+3. When a turn is detected, vehicle needs to change PWM to change angle according to white line.
+  3a. If left turn detected, right wheel needs to slow down and vice versa.
+  3b. This could be done with an error variable, if larger than 0, we need to turn right and if less than 0, we need to turn left.
+
+----
+*/
