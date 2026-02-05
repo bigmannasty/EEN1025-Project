da052d82cd88025c31ad534d0e8976c6261b86c8
bigmannasty <admirmen@gmail.com>
2026-02-05 16:07:00 +0000

    Update MotorLineSensor.ino

--- MotorLineSensor/MotorLineSensor.ino
+++ MotorLineSensor/MotorLineSensor.ino
@@ -9,15 +9,18 @@ const int motor2Phase = 40;
 
 // Added these global variables
 String route = "";  // <--- ADDED: Store the route received from GET
-int routeList[] = {0, 1, 3, 4, 5};
+int routeList[] = {0, 3, 1, 4, 1, 2, 1, 3, 5};
 int position = 0;  // <--- ADDED: Track current position index
 bool routeCompleted = false;  // <--- ADDED: Flag for route completion
 
 
 //node and wight arrays
-int nodes[][5] = {{0, 1, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 0, 1, 0}, {0, 1, 1, 0, 1}, {1, 1, 0, 1, 0}};
-int nodeWeights[][5] = {{9, 2, 2, 9, 1}, {2, 9, 2, 4, 4}, {2, 2, 9, 1, 9}, {9, 4, 1, 9, 5}, {1, 4, 9, 5, 9}};
-int nodeDirection[][5] = {{9, 1, 1, 9, 0}, {0, 9, 0, 1, 1}, {0, 0, 9, 1, 9}, {9, 1, 0, 9, 1}, {1, 0, 9, 0, 9}}; // 0 clockwise  1 anti clockwise
+int nodes[][5] = {{0, 1, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 0, 1, 0}, {0, 1, 1, 0, 1}, {1, 1, 0, 1, 0}}; // direct node connections, 0 - no direct connection, 1 - direct connection
+int nodeWeights[][5] = {{9, 2, 2, 9, 1}, {2, 9, 2, 4, 4}, {2, 2, 9, 1, 9}, {9, 4, 1, 9, 6}, {1, 4, 9, 6, 9}}; // route weightings for each node
+int nodeDirection[][5] = {{9, 1, 1, 9, 0}, {0, 9, 1, 0, 1}, {0, 0, 9, 1, 9}, {9, 1, 0, 9, 1}, {1, 0, 9, 0, 9}}; // 0 clockwise  1 anti clockwise
+
+int nodePathRank03[3] = {2, 1, 4}; // route paths for node 0 -> 3 and vice versa
+int nodePathRank24[3] = {0, 1, 3}; // route paths for node 2 -> 4 and vice versa
 
 
 
@@ -133,7 +136,7 @@ void motorTurn(int dir) {
 
 
 // function to do a 180 
-void motor180() {
+void motor180(int startPos) {
   motorDrive(100,100);//move forward a bit before turn
   delay(50);
   digitalWrite(motor1Phase, HIGH);
@@ -148,8 +151,10 @@ void motor180() {
   }
   motorDrive(0, 0);
   motorDir(0);
-  if (currentDir == 0) { currentDir = 1; } // flip direction
-  else { currentDir = 0; }
+  if (startPos != 1) {
+    if (currentDir == 0) { currentDir = 1; } // flip direction
+    else { currentDir = 0; }
+  }
 }
 
 
@@ -269,11 +274,24 @@ void setup() {
   lineDetected = false;
   // Initialize motor
   initMotor();
-  motorDir(0);
-  motorDrive(speedL, speedR);
-  delay(100);
+  //motorDir(0);
+  //motorDrive(speedL, speedR);
+  //delay(100);
+
+
+/*
+  for (int i; routeList[i+1] != 5; i++) {
+    int node1 = routeList[i];
+    int node2 = routeList[i+1];
+    if (nodes[node1][node2] == 0) {
+      if (node1 == 0 || node1 == 3) {}
+      else if (node1 == 2 || node1 == 4) {}
+    }
+  }
+  */
   
 }
+
 
 
 
@@ -337,18 +355,22 @@ void loop() {
   if (nodeDetected == true) { // if reached a node
 
     motorDrive(0,0);
-    delay(500);
+    delay(50);
 
     if (passedJunc == true) { // if node is not a junction
 
-      motorDrive(0,0);
+      //motorDrive(0,0);
       
       if (nextPos == routeList[currentRouteNodeIndex]) { // if the node youre at right now is an actual route node
         currentRouteNodeIndex++; // move onto the next routelist node
         startPos = nextPos; // new start node becomes the last dest node
         nextPos = routeList[currentRouteNodeIndex]; // new next node becomes the next required destination node in the route list
-        routeFind(); // find the new route for the current start and next nodes
-        updateNextPos();
+        if (nodes[startPos][nextPos] == 0) {
+          if (startPos == 0 || startPos == 3) { nextPos = nodePathRank03[currentNodeRank]; }
+          else { nextPos = nodePathRank24[currentNodeRank]; }
+        }
+        //routeFind(); // find the new route for the current start and next nodes
+        //updateNextPos();
         passedJunc = false;
         motorDrive(speedL, speedR);
         while (nodeDetected) { // while still on node 
@@ -380,12 +402,31 @@ void loop() {
         startPos = nextPos; // reached node is new start
         nextPos = routeList[currentRouteNodeIndex]; // nextpos becomes the original main destination node from the routelist
         passedJunc = false; // reset junction var
+        motorDrive(speedL, speedR);
+        while (nodeDetected) { // while still on node 
+          activeSensors = 0;
+          for (int i = 0; i < 5; i++) {
+            lineValue[i] = analogRead(lineSensePin[i]);
+            if (lineValue[i] <= WHITE_THRESHOLD) {
+                error += weights[i];
+                activeSensors++;
+            }
+          }
+          if (activeSensors < 3) {nodeDetected = false;}
+        }
         Serial.println("sub route loop");
 
       }
 
+      Serial.print("Start: ");
+      Serial.println(startPos);
+      Serial.print("Next: ");
+      Serial.println(nextPos);
+
       if ( (startPos == 0 && nextPos == 4) || (startPos == 4 && nextPos == 0) ) { passedJunc = true; } // if starts at 0 and goes 4 or vice versa, next node is not junction
       else if ( (startPos == 2 && nextPos == 3) || (startPos == 3 && nextPos == 2) ) { passedJunc = true; } // if starts at 2 and goes 3 or vice versa, next node is not junction
+
+      if (currentDir != nodeDirection[startPos][nextPos]) { motor180(startPos); }
 
 
     }
@@ -397,27 +438,44 @@ void loop() {
 
       Serial.println("at junc loop");
 
+      motorDrive(speedL, speedR);
+      while (nodeDetected) { // while still on node 
+        activeSensors = 0;
+        for (int i = 0; i < 5; i++) {
+          lineValue[i] = analogRead(lineSensePin[i]);
+          if (lineValue[i] <= WHITE_THRESHOLD) {
+              error += weights[i];
+              activeSensors++;
+          }
+        }
+        if (activeSensors < 3) {nodeDetected = false;}
+      }
+
       if (currentDir == 0 && nextPos == 1) { // if going clockwise to node 1
         motorTurn(0); // turn right at the next junction
         Serial.println("turn right");
+        currentDir = 1;
       }
 
       else if (currentDir == 1 && nextPos == 1) { // if going anti-clockwise to node 1
         motorTurn(1); // turn left at the next junction
         Serial.println("turn left");
+        currentDir = 0;
       }
 
 
       if (startPos == 1 && (nextPos == 2 || nextPos == 4)) { // if going from node 1 to node 2 or 4
         motorTurn(1); // turn left at the next junction
         Serial.println("turn left");
-        currentDir = 1;
+        if (nextPos == 2) { currentDir = 1; }
+        else { currentDir = 1; }
       }
 
       else if (startPos == 1 && (nextPos == 0 || nextPos == 3)) { // if going from node 1 to node 0 or 3
         motorTurn(0); // turn righ at the next junction
         Serial.println("turn right");
-        currentDir = 0;
+        if (nextPos == 0) { currentDir = 0; }
+        else { currentDir = 0; }
       }
 
       passedJunc = true; // indicate that the junction has been passed
@@ -427,7 +485,7 @@ void loop() {
 
   }
 
-  if (currentDir != nodeDirection[startPos][nextPos]) { motor180(); }
+  
 
 
 
