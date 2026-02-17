6736f8ccb35d776f3b8bba59ab54d0e5fbaea7a3
ZygiGricius <zygimantas.gricius2@mail.dcu.ie>
2026-02-17 10:14:31 +0000

    added millis implementation for display

--- displayTest/displayTest.ino
+++ displayTest/displayTest.ino
@@ -9,15 +9,16 @@ const short I2C_SDA = 8;
 const short I2C_SCL = 9;
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
 
-const short button = 1;
+enum displayState{
+  TEXT,
+  IDLE,
+  NODE
+};
+
+displayState currDisplayState = TEXT;
 
 short node = -1;
 const short route[] = {0, 1, 3, 2, 5};
-
-int buttonState = LOW;
-int lastState = LOW;
-unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay = 50;
 
 //Function to Draw arrow on display
 void drawArrow32x16(int x, int y) {
@@ -42,31 +43,11 @@ void drawArrow32x16(int x, int y) {
     SSD1306_WHITE
   );
 }
-
-void setup() {
-  Serial.begin(115200);
-
-  // Start I2C on chosen pins
-  Wire.begin(I2C_SDA, I2C_SCL);
-  pinMode(button, INPUT_PULLDOWN);
-  //attachInterrupt(button, handleButtonISR, FALLING);
-
-  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
-    Serial.println("SSD1306 allocation failed");
-    while (true);
-  }
-  display.clearDisplay();
-  display.setTextSize(4);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 16);
-  display.print("START");
-  display.display();
-}
-
 const short arrowX = 48;
 const short arrowY = 24;
-
 const short centre = 8;
+short scrollVertical = 8;
+const int uiInterval = 1;
 
 void updateUI(short node) {
   short scrollVertical = 8;
@@ -98,19 +79,118 @@ void updateUI(short node) {
   }
 }
 
-void loop() {
+unsigned long lastDisplayUpdate = 0;
 
-  for (int i = 0; i < 5; i++)
+
+short currentNode = 5;
+short nextNode = 0;
+void nodeUpdate() {
+  unsigned long currentMillis = millis();
+
+  //Check if enough time has elapsed to update animation
+  if (currentMillis - lastDisplayUpdate >= uiInterval)
   {
-    updateUI(i);
-    delay(150);
+    lastDisplayUpdate = currentMillis;
+
+    //Draws arrow
+    display.clearDisplay();
+    drawArrow32x16(arrowX, arrowY);
+
+    //Draws Scrolling Text
+    display.setTextSize(7);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(4, scrollVertical);
+    display.print(currentNode);
+    display.setCursor(86, scrollVertical);
+    display.print(nextNode);
+    display.display();
+
+    //Decrement y pos of node numbers
+    scrollVertical -=4;
+
+    //If scrolled to the top, jump to the bottom
+    if (scrollVertical < -64 && scrollVertical > -100) {
+      scrollVertical = 64;
+    }
+
+    if (scrollVertical == 8) {
+      currDisplayState = IDLE;
+      currentNode = nextNode;
+    }
   }
 }
-/*
-TO DO:
-1a. Displays PARK when ending route and plays music from buzzer
-1b. Once parked, display END
 
-2. At each node, plays buzzer sound relating to node number
-*/
+void startNodeUpdate(short node) {
+  nextNode = node;
+  scrollVertical = 4;
+  lastDisplayUpdate = millis();
+  currDisplayState = NODE;
+}
 
+bool parked = false;
+void textUpdate() {
+  //Will display start text
+  if (parked == false)
+  {
+    display.clearDisplay();
+    display.setTextColor(SSD1306_WHITE);
+    display.setTextSize(4);
+    display.setCursor(0, 16);
+    display.print("START");
+    display.display();
+  }
+  else
+  {
+    display.clearDisplay();
+    display.setTextColor(SSD1306_WHITE);
+    display.setTextSize(5);
+    display.setCursor(0, 16);
+    display.print("PARK");
+    display.display();
+  }
+}
+void startText() {
+  if (currentNode == 5)
+  {
+    parked = true;
+    currDisplayState = TEXT;
+  }
+  else
+  {
+    currDisplayState = TEXT;
+  }
+}
+
+void updateDisplay() {
+  switch(currDisplayState)
+  {
+    case IDLE:
+      break;
+    case NODE:
+      nodeUpdate();
+      break;
+    case TEXT:
+      textUpdate();
+      break;
+  }
+}
+
+
+void setup() {
+  Serial.begin(115200);
+
+  // Start I2C on chosen pins
+  Wire.begin(I2C_SDA, I2C_SCL);
+
+  //Prevents ESP32 from working if display setup has failed
+  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+    Serial.println("SSD1306 allocation failed");
+    while (true);
+  }
+  //startNodeUpdate(2);
+  startText();
+}
+
+void loop() {
+  updateDisplay();
+}
